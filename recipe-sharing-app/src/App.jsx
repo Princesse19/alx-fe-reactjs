83caf5367697ee73ba3e0517822741d4a708877a
Princesse19 <munyanezaprincesserita@gmail.com>
2025-07-30 08:59:20 -0700

    Add DeleteRecipeButton and RecipeDetails components, fix imports, and clean up

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,14 +1,31 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
+import RecipeDetails from './components/RecipeDetails';
 
 function App() {
   return (
-    <div>
+    <Router>
       <h1>Recipe Sharing App</h1>
-      <AddRecipeForm />
-      <RecipeList />
-    </div>
+      <Routes>
+        <Route path="/" element={
+          <>
+            <AddRecipeForm />
+            <RecipeList />
+          </>
+        } />
+        <Route path="/recipe/:id" element={<RecipeDetailsWrapper />} />
+      </Routes>
+    </Router>
   );
 }
 
+import { useParams } from 'react-router-dom';
+const RecipeDetailsWrapper = () => {
+  const { id } = useParams();
+  const recipeId = Number(id);
+  return <RecipeDetails recipeId={recipeId} />;
+};
+
 export default App;
+
