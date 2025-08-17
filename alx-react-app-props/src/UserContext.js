31e185433de006b56b58f7272bbf3e4807f8b0d1
Princesse19 <munyanezaprincesserita@gmail.com>
2025-08-17 13:24:58 -0700

    Task 2: Add Context API and fix UserContext.js checker issue

--- alx-react-app-props/src/UserContext.js
+++ alx-react-app-props/src/UserContext.js
@@ -1,6 +1,11 @@
+// UserContext.js
+// Context to share user data across components in the app
+
 import { createContext } from 'react';
 
+// Create a context with default null value
 const UserContext = createContext(null);
 
+// Export the context so other components can import it
 export default UserContext;
 
