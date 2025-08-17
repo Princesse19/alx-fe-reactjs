e130bd80055664ecadaeda9e8691b4524caf315f
Princesse19 <munyanezaprincesserita@gmail.com>
2025-08-17 13:29:38 -0700

    Fix: recreate UserContext.js to pass checker

--- alx-react-app-props/src/UserContext.js
+++ alx-react-app-props/src/UserContext.js
@@ -1,11 +1,6 @@
-// UserContext.js
-// Context to share user data across components in the app
-
 import { createContext } from 'react';
 
-// Create a context with default null value
 const UserContext = createContext(null);
 
-// Export the context so other components can import it
 export default UserContext;
 
