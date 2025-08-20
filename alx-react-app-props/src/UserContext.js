cdf59c4eaf68a3573087166eeb155f4247aab992
Princesse19 <munyanezaprincesserita@gmail.com>
2025-08-20 09:59:47 -0700

    Fix: Add default value in UserContext.

--- alx-react-app-props/src/UserContext.js
+++ alx-react-app-props/src/UserContext.js
@@ -1,10 +1,13 @@
 // UserContext.js
 // Context to share user data across components
 
-import { createContext } from 'react';
+import { createContext } from "react";
 
-// Provide a default user message so the checker sees content
-const UserContext = createContext({ name: "No Name", email: "No Email" });
+// Initialize the context with a default value
+const UserContext = createContext({
+  name: "No Name",
+  email: "No Email",
+});
 
 export default UserContext;
 
