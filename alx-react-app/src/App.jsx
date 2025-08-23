82bdc411c588aefc5adca1138d5868bccf87ee2d
Princesse19 <munyanezaprincesserita@gmail.com>
2025-08-23 07:52:36 -0700

    Task 2: Created Header, MainContent, and Footer components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,13 +1,21 @@
 import WelcomeMessage from './components/WelcomeMessage';
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
 import './App.css';
 
 function App() {
   return (
     <div>
+      {/* Task 1 component */}
       <WelcomeMessage />
+
+      {/* Task 2 components */}
+      <Header />
+      <MainContent />
+      <Footer />
     </div>
   );
 }
 
 export default App;
-
