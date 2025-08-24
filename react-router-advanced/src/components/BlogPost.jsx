709173362704ad37a88b4374030007015b616ee7
Princesse19 <munyanezaprincesserita@gmail.com>
2025-08-24 15:18:57 -0700

    Add dynamic route /blog/:id with BlogPost component

---
+++ react-router-advanced/src/components/BlogPost.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+function BlogPost() {
+  const { id } = useParams();
+  return <h1>Blog Post ID: {id}</h1>;
+}
+
+export default BlogPost;
+
